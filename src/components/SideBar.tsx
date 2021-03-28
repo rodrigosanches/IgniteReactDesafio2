d7d85bf9ee5e89b6d7eca70548727b5bf9825886
sanchesromao@gmail.com <sanchesromao@gmail.com>
2021-03-28 17:45:42 -0300

    Desafio 2 - Ignite React

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,58 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+import { api } from '../services/api';
+
+interface SideBarProps {
+  itenselected: number;
+  onSetSelectedGenreId: (id: number) => void;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
+  const {
+    itenselected: selectedGenreId,
+    onSetSelectedGenreId: setSelectedGenreId,
+  } = props;
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
